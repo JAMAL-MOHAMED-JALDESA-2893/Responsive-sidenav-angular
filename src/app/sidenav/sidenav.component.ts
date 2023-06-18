d9d320d4d39d53911e148026151b2704451537ea
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-06-18 09:43:24 +0300

    created navbar data ts file and imported it in sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { navbarData } from './nav-data';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class SidenavComponent {
 
+  collapsed = false;
+  navData = navbarData;
 }
