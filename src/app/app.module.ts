effaad636df5aec31b079508abbf85fccb3f343c
JAMAL-MOHAMED-JALDESA-2893 <jayhamo22@gmail.com>
2023-06-18 09:11:41 +0300

    add media component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { StatisticsComponent } from './statistics/statistics.component';
 import { CoupensComponent } from './coupens/coupens.component';
 import { PagesComponent } from './pages/pages.component';
 import { MediaComponent } from './media/media.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { MediaComponent } from './media/media.component';
     StatisticsComponent,
     CoupensComponent,
     PagesComponent,
-    MediaComponent
+    MediaComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
